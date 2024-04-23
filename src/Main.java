ff61d0fb796a483dde8d8604795d0468616ea8de
GitOnat <maon0001@stud.kea.dk>
2024-04-23 14:55:19 +0200

    cat dog in main makeSound

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,10 @@
 public class Main {
     public static void main(String[] args) {
 
+        Animal myDog = new Dog();
+        Animal myCat = new Cat();
+
+        myDog.makeSound();
+        myCat.makeSound();
     }
 }
